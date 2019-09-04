2d3b725c22c228075b23b42de9508ece121eeac1
Link <37621328+linyink@users.noreply.github.com>
2019-09-04 09:29:01 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ DEBUG = True
 # 安全密码一定要是字符串类型
 SECRET_KEY = '123456'
 # 在数据库迁移时遇到一个问题，SQLALCHEMY_DATABASE_URI中的值报错
-SQLALCHEMY_DATABASE_URI = "mysql://root:123456@localhost/web_travel"
+SQLALCHEMY_DATABASE_URI = "mysql://username:password@localhost/web_travel"  # 需要改成你自己的
 SQLALCHEMY_TRACK_MODIFICATIONS = False
